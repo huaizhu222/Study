f24f8d9bb33c8d7a28d6fc2ed757ea63fb53cf09
rzw <1945550519@qq.com>
2025-12-28 19:49:13 +0800

    添加返回操作

--- simple_gin/main.go
+++ simple_gin/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"Rpc.Study.go/simple_gin/gee"
@@ -9,14 +8,10 @@ import (
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-	})
-
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/", func(ctx *gee.Context) {
+		ctx.JSON(http.StatusOK, map[string]string{
+			"你好": "hello world",
+		})
 	})
 
 	r.Run(":9999")
